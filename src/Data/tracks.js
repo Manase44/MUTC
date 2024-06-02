bf024b62f4b68c011adf2a97435a0c99fc80e96d
Manase44 <fikopersempre@gail.com>
2024-06-02 23:54:23 +0300

    add track pictures

--- src/Data/tracks.js
+++ src/Data/tracks.js
@@ -1,9 +1,9 @@
 import cyberImage from "../assets/Images/tracks/cybersecurity.webp"
 import cloudImage from "../assets/Images/tracks/cloudengineering.webp"
-import cyberImage from "../assets/Images/tracks/cybersecurity.webp"
-import cyberImage from "../assets/Images/tracks/cybersecurity.webp"
-import cyberImage from "../assets/Images/tracks/cybersecurity.webp"
-import cyberImage from "../assets/Images/tracks/cybersecurity.webp"
+import mobileImage from "../assets/Images/tracks/mobile.webp"
+import powerImage from "../assets/Images/tracks/powerplatform.webp"
+import uiuxImage from "../assets/Images/tracks/ui-ux.webp"
+import webImage from "../assets/Images/tracks/webdev.webp"
 const tracks = [
   {
     id: "track001",
@@ -14,21 +14,21 @@ const tracks = [
   },
   {
     id: "track002",
-    image: "",
+    image: webImage,
     name: "web development",
     description:
       "Help students to gain comprehensive knowledge in building dynamic and responsive websites. They learn the fundamentals of front-end development using HTML, CSS, and JavaScript, as well as back-end development with technologies such as Node.js, Express, and databases. Through hands-on projects and collaborative coding sessions, students develop the skills to create both the client-side and server-side of web applications. By the end of the track, students have the capability of designing, developing, and deploying full-stack web applications,as well as building robust and scalable websites.",
   },
   {
     id: "track003",
-    image: "",
+    image: uiuxImage,
     name: "UI/UX design",
     description:
       "This track explores the principles of creating user-friendly and visually appealing interfaces. Students learn about user research, wireframing, prototyping, and usability testing. Through practical projects and interactive workshops, they gain hands-on experience in designing intuitive user interfaces and enhancing user experiences. By the end of the track, students are proficient in using industry-standard design tools and techniques, enabling them to create effective, user-centric designs for websites and mobile applications.",
   },
   {
     id: "track004",
-    image: "",
+    image: mobileImage,
     name: "mobile application development",
     description:
       "This track helps learners to learn how to design and build applications for mobile devices. They explore the basics of mobile app development using platforms such as Android and iOS, gaining proficiency in languages like Java, Kotlin, Swift, and Dart. Through hands-on projects and interactive sessions, they develop the skills to create user-friendly and efficient mobile applications. By the end of the track, they are adept at building, testing, and deploying mobile apps, ready to deliver engaging and high-performance solutions for smartphones and tablets.",
@@ -42,7 +42,7 @@ const tracks = [
   },
   {
     id: "track006",
-    image: "",
+    image: powerImage,
     name: "power platform",
     description:
       "Enables learners to explore the capabilities of Microsoft's Power Platform, which includes Power BI, Power Apps, Power Automate, and Power Virtual Agents. They,  learn how to create custom business applications, automate workflows, analyze data, and build chatbots with minimal coding. Through hands-on projects and interactive workshops, they gain practical experience in leveraging these tools to enhance business processes and drive data-driven decision-making. By the end of the track, they are proficient in developing integrated solutions using the Power Platform, enabling them to transform organizational operations and improve efficiency.",
