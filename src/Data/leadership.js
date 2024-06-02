df68971bdefb14167855fa1780d90c4c90766096
Manase44 <fikopersempre@gail.com>
2024-06-02 22:05:59 +0300

    add the leader picture

--- src/Data/leadership.js
+++ src/Data/leadership.js
@@ -1,107 +1,130 @@
-const leadership = [
-    {
-        id: "leader01",
-        name: "Dr. John Ndia",
-        position: "Patron",
-        linkedIn: "https://www.linkedin.com/in/manasegunga/",
-        twitter: "https://x.com/",
-        facebook: "https://web.facebook.com/?_rdc=1&_rdr",
-        biography: ""
-    },
-    {
-        id: "leader02",
-        name: "Victory Njeri",
-        position: "Chairperson",
-        linkedIn: "https://www.linkedin.com/in/manasegunga/",
-        twitter: "https://x.com/",
-        facebook: "https://web.facebook.com/?_rdc=1&_rdr",
-        instagram: "https://www.instagram.com/fikopersempre/",
-        biography: ""
-    },
-    {
-        id: "leader03",
-        name: "Allan Muhari",
-        position: "Vice Chairperson",
-        linkedIn: "https://www.linkedin.com/in/manasegunga/",
-        twitter: "https://x.com/",
-        biography: ""
-    },
-    {
-        id: "leader04",
-        name: "Bridget Gitonga",
-        position: "secretary",
-        linkedIn: "https://www.linkedin.com/in/manasegunga/",
-        twitter: "https://x.com/",
-        biography: ""
-    },
-    {
-        id: "leader05",
-        name: "Ruth Mutisya",
-        position: "treasurer",
-        linkedIn: "https://www.linkedin.com/in/manasegunga/",
-        instagram: "https://www.instagram.com/fikopersempre/",
-        biography: ""
-    },
-    {
-        id: "leader06",
-        name: "Webster Ifedha",
-        position: "cyber security lead",
-        linkedIn: "https://www.linkedin.com/in/manasegunga/",
-        twitter: "https://x.com/",
-        facebook: "https://web.facebook.com/?_rdc=1&_rdr",
-        instagram: "https://www.instagram.com/fikopersempre/",
-        biography: ""
-    },
-    {
-        id: "leader07",
-        name: "Manase Gunga ",
-        position: "UI/UX lead",
-        linkedIn: "https://www.linkedin.com/in/manasegunga/",
-        twitter: "https://x.com/",
-        facebook: "https://web.facebook.com/?_rdc=1&_rdr",
-        instagram: "https://www.instagram.com/fikopersempre/",
-        biography: ""
-    },
-    {
-        id: "leader08",
-        name: "Carolyne Githenduka",
-        position: "web development lead.",
-        linkedIn: "https://www.linkedin.com/in/manasegunga/",
-        twitter: "https://x.com/",
-        facebook: "https://web.facebook.com/?_rdc=1&_rdr",
-        instagram: "https://www.instagram.com/fikopersempre/",
-        biography: ""
-    },
-    {
-        id: "leader09",
-        name: "Stanley Amunze",
-        position: "Mobile apps development lead",
-        linkedIn: "https://www.linkedin.com/in/manasegunga/",
-        twitter: "https://x.com/",
-        facebook: "https://web.facebook.com/?_rdc=1&_rdr",
-        instagram: "https://www.instagram.com/fikopersempre/",
-        biography: ""
-    },
-    {
-        id: "leader10",
-        name: "Paul Karanja",
-        position: "cloud engineering lead",
-        linkedIn: "https://www.linkedin.com/in/manasegunga/",
-        twitter: "https://x.com/",
-        facebook: "https://web.facebook.com/?_rdc=1&_rdr",
-        instagram: "https://www.instagram.com/fikopersempre/",
-        biography: ""
-    },
-    {
-        id: "leader11",
-        name: "Evyonn Mbithe",
-        position: "power platform lead",
-        linkedIn: "https://www.linkedin.com/in/manasegunga/",
-        twitter: "https://x.com/",
-        facebook: "https://web.facebook.com/?_rdc=1&_rdr",
-        instagram: "https://www.instagram.com/fikopersempre/",
-        biography: ""
-    }
-]
+import allanImage from "../assets/Images/leaders/allan.webp";
+import bridgetImage from "../assets/Images/leaders/bridget.webp";
+import carolyneImage from "../assets/Images/leaders/carolyne.webp";
+import evyonnImage from "../assets/Images/leaders/evyon.webp";
+import johnImage from "../assets/Images/leaders/john.webp";
+import manaseImage from "../assets/Images/leaders/manase.webp";
+import paulImage from "../assets/Images/leaders/paul.webp";
+import ruthImage from "../assets/Images/leaders/ruth.webp";
+import stanleyImage from "../assets/Images/leaders/stanley.webp";
+import victorImage from "../assets/Images/leaders/victor.webp";
+import websterImage from "../assets/Images/leaders/webster.webp";
 
-export default leadership
+const leadership = [
+  {
+    id: "leader01",
+    name: "Dr. John Ndia",
+    image: johnImage,
+    position: "Patron",
+    linkedIn: "https://www.linkedin.com/in/manasegunga/",
+    twitter: "https://x.com/",
+    facebook: "https://web.facebook.com/?_rdc=1&_rdr",
+    biography: "",
+  },
+  {
+    id: "leader02",
+    name: "Victory Njeri",
+    image: victorImage,
+    position: "Chairperson",
+    linkedIn: "https://www.linkedin.com/in/manasegunga/",
+    twitter: "https://x.com/",
+    facebook: "https://web.facebook.com/?_rdc=1&_rdr",
+    instagram: "https://www.instagram.com/fikopersempre/",
+    biography: "",
+  },
+  {
+    id: "leader03",
+    name: "Allan Muhari",
+    image: allanImage,
+    position: "Vice Chairperson",
+    linkedIn: "https://www.linkedin.com/in/manasegunga/",
+    twitter: "https://x.com/",
+    biography: "",
+  },
+  {
+    id: "leader04",
+    name: "Bridget Gitonga",
+    image: bridgetImage,
+    position: "secretary",
+    linkedIn: "https://www.linkedin.com/in/manasegunga/",
+    twitter: "https://x.com/",
+    biography: "",
+  },
+  {
+    id: "leader05",
+    name: "Ruth Mutisya",
+    image: ruthImage,
+    position: "treasurer",
+    linkedIn: "https://www.linkedin.com/in/manasegunga/",
+    instagram: "https://www.instagram.com/fikopersempre/",
+    biography: "",
+  },
+  {
+    id: "leader06",
+    name: "Webster Ifedha",
+    image: websterImage,
+    position: "cyber security lead",
+    linkedIn: "https://www.linkedin.com/in/manasegunga/",
+    twitter: "https://x.com/",
+    facebook: "https://web.facebook.com/?_rdc=1&_rdr",
+    instagram: "https://www.instagram.com/fikopersempre/",
+    biography: "",
+  },
+  {
+    id: "leader07",
+    name: "Manase Gunga ",
+    image: manaseImage,
+    position: "UI/UX lead",
+    linkedIn: "https://www.linkedin.com/in/manasegunga/",
+    twitter: "https://x.com/",
+    facebook: "https://web.facebook.com/?_rdc=1&_rdr",
+    instagram: "https://www.instagram.com/fikopersempre/",
+    biography: "",
+  },
+  {
+    id: "leader08",
+    name: "Carolyne Githenduka",
+    image: carolyneImage,
+    position: "web development lead.",
+    linkedIn: "https://www.linkedin.com/in/manasegunga/",
+    twitter: "https://x.com/",
+    facebook: "https://web.facebook.com/?_rdc=1&_rdr",
+    instagram: "https://www.instagram.com/fikopersempre/",
+    biography: "",
+  },
+  {
+    id: "leader09",
+    name: "Stanley Amunze",
+    image: stanleyImage,
+    position: "Mobile apps development lead",
+    linkedIn: "https://www.linkedin.com/in/manasegunga/",
+    twitter: "https://x.com/",
+    facebook: "https://web.facebook.com/?_rdc=1&_rdr",
+    instagram: "https://www.instagram.com/fikopersempre/",
+    biography: "",
+  },
+  {
+    id: "leader10",
+    name: "Paul Karanja",
+    image: paulImage,
+    position: "cloud engineering lead",
+    linkedIn: "https://www.linkedin.com/in/manasegunga/",
+    twitter: "https://x.com/",
+    facebook: "https://web.facebook.com/?_rdc=1&_rdr",
+    instagram: "https://www.instagram.com/fikopersempre/",
+    biography: "",
+  },
+  {
+    id: "leader11",
+    name: "Evyonn Mbithe",
+    image: evyonnImage,
+    position: "power platform lead",
+    linkedIn: "https://www.linkedin.com/in/manasegunga/",
+    twitter: "https://x.com/",
+    facebook: "https://web.facebook.com/?_rdc=1&_rdr",
+    instagram: "https://www.instagram.com/fikopersempre/",
+    biography: "",
+  },
+];
+
+export default leadership;
