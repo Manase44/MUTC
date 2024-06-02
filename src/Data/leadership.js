02eb1063c85a48659ed8fbd377d6baf335af133f
Manase44 <fikopersempre@gail.com>
2024-06-02 21:31:21 +0300

    add the leadership section

---
+++ src/Data/leadership.js
@@ -0,0 +1,107 @@
+const leadership = [
+    {
+        id: "leader01",
+        name: "Dr. John Ndia",
+        position: "Patron",
+        linkedIn: "https://www.linkedin.com/in/manasegunga/",
+        twitter: "https://x.com/",
+        facebook: "https://web.facebook.com/?_rdc=1&_rdr",
+        biography: ""
+    },
+    {
+        id: "leader02",
+        name: "Victory Njeri",
+        position: "Chairperson",
+        linkedIn: "https://www.linkedin.com/in/manasegunga/",
+        twitter: "https://x.com/",
+        facebook: "https://web.facebook.com/?_rdc=1&_rdr",
+        instagram: "https://www.instagram.com/fikopersempre/",
+        biography: ""
+    },
+    {
+        id: "leader03",
+        name: "Allan Muhari",
+        position: "Vice Chairperson",
+        linkedIn: "https://www.linkedin.com/in/manasegunga/",
+        twitter: "https://x.com/",
+        biography: ""
+    },
+    {
+        id: "leader04",
+        name: "Bridget Gitonga",
+        position: "secretary",
+        linkedIn: "https://www.linkedin.com/in/manasegunga/",
+        twitter: "https://x.com/",
+        biography: ""
+    },
+    {
+        id: "leader05",
+        name: "Ruth Mutisya",
+        position: "treasurer",
+        linkedIn: "https://www.linkedin.com/in/manasegunga/",
+        instagram: "https://www.instagram.com/fikopersempre/",
+        biography: ""
+    },
+    {
+        id: "leader06",
+        name: "Webster Ifedha",
+        position: "cyber security lead",
+        linkedIn: "https://www.linkedin.com/in/manasegunga/",
+        twitter: "https://x.com/",
+        facebook: "https://web.facebook.com/?_rdc=1&_rdr",
+        instagram: "https://www.instagram.com/fikopersempre/",
+        biography: ""
+    },
+    {
+        id: "leader07",
+        name: "Manase Gunga ",
+        position: "UI/UX lead",
+        linkedIn: "https://www.linkedin.com/in/manasegunga/",
+        twitter: "https://x.com/",
+        facebook: "https://web.facebook.com/?_rdc=1&_rdr",
+        instagram: "https://www.instagram.com/fikopersempre/",
+        biography: ""
+    },
+    {
+        id: "leader08",
+        name: "Carolyne Githenduka",
+        position: "web development lead.",
+        linkedIn: "https://www.linkedin.com/in/manasegunga/",
+        twitter: "https://x.com/",
+        facebook: "https://web.facebook.com/?_rdc=1&_rdr",
+        instagram: "https://www.instagram.com/fikopersempre/",
+        biography: ""
+    },
+    {
+        id: "leader09",
+        name: "Stanley Amunze",
+        position: "Mobile apps development lead",
+        linkedIn: "https://www.linkedin.com/in/manasegunga/",
+        twitter: "https://x.com/",
+        facebook: "https://web.facebook.com/?_rdc=1&_rdr",
+        instagram: "https://www.instagram.com/fikopersempre/",
+        biography: ""
+    },
+    {
+        id: "leader10",
+        name: "Paul Karanja",
+        position: "cloud engineering lead",
+        linkedIn: "https://www.linkedin.com/in/manasegunga/",
+        twitter: "https://x.com/",
+        facebook: "https://web.facebook.com/?_rdc=1&_rdr",
+        instagram: "https://www.instagram.com/fikopersempre/",
+        biography: ""
+    },
+    {
+        id: "leader11",
+        name: "Evyonn Mbithe",
+        position: "power platform lead",
+        linkedIn: "https://www.linkedin.com/in/manasegunga/",
+        twitter: "https://x.com/",
+        facebook: "https://web.facebook.com/?_rdc=1&_rdr",
+        instagram: "https://www.instagram.com/fikopersempre/",
+        biography: ""
+    }
+]
+
+export default leadership
--- src/Pages/Leadership/Leardership.jsx
+++ src/Pages/Leadership/Leardership.jsx
@@ -1,5 +1,47 @@
+import "./Leadership.css";
+import leadership from "../../Data/leadership";
+import leaderImage from "../../assets/Images/fatuma.jpg"
+import { FaFacebook } from "react-icons/fa";
+import { RiInstagramFill } from "react-icons/ri";
+import { AiFillTwitterCircle } from "react-icons/ai";
+import { FaLinkedin } from "react-icons/fa6";
+
+
+
 const Leardership = () => {
-  return <div>Leardership</div>;
+  return (
+    <section className="leadership-section" id="leadership">
+      <h1 className="section-title">leadership</h1>
+      <div className="leadership-container">
+        { leadership.map((currentleader) => {
+          return (
+            <div className="leader-card">
+          <div className="leader-image">
+            <img src={leaderImage} alt="A leader " />
+            <div className="socials">
+              <ul>
+                <li><a href={currentleader.facebook} target="-blank">{currentleader.facebook&&<FaFacebook/>}</a></li>
+                <li><a href={currentleader.twitter} target="-blank">{currentleader.twitter&&<AiFillTwitterCircle/>}</a></li>
+                <li><a href={currentleader.linkedIn} target="-blank">{currentleader.linkedIn&&<FaLinkedin/>}</a></li>
+                <li><a href={currentleader.instagram} target="-blank">{currentleader.instagram&&<RiInstagramFill/>}</a></li>
+              </ul>
+              <p className="follow-text">follow</p>
+              <div className="line"></div>
+            </div>
+          </div>
+          <div className="leader-details">
+            <p className="leader-name">{currentleader.name}</p>
+            <p className="leader-position">{currentleader.position}</p>
+            <p className="leader-bio">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum reiciendis eligendi saepe odio ea deleniti soluta qui praesentium illo dolore.
+            </p>
+          </div>
+        </div>
+          )
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Leardership;
