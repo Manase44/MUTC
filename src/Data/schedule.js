7e1e852c7986c9a8613952fe63d12f84ff0281b9
Manase44 <fikopersempre@gail.com>
2024-06-02 19:11:48 +0300

    add the schedule under event page

---
+++ src/Data/schedule.js
@@ -0,0 +1,46 @@
+const schedule = [
+  {
+    id: 1,
+    day: "monday",
+    title: "cyber security",
+    time: "4pm to 7pm",
+    by: "Webster Ifedha",
+  },
+  {
+    id: 2,
+    day: "tuesday",
+    title: "UI/UX design",
+    time: "5pm to 8pm",
+    by: "Manase Gunga",
+  },
+  {
+    id: 3,
+    day: "wednessday",
+    title: "web development",
+    time: "6pm to 9pm",
+    by: "Carolyne Githenduka",
+  },
+  {
+    id: 4,
+    day: "thursday",
+    title: "mobile app development",
+    time: "4pm to 7pm",
+    by: "Stanley Amunze",
+  },
+  {
+    id: 5,
+    day: "friday",
+    title: "cloud engineering",
+    time: "3pm to 6pm",
+    by: "Paul Karanja",
+  },
+  {
+    id: 6,
+    day: "saturday",
+    title: "power platform",
+    time: "1pm to 4pm",
+    by: "Evyonn Mbithe",
+  },
+];
+
+export default schedule;
--- src/Pages/Event/Events.jsx
+++ src/Pages/Event/Events.jsx
@@ -1,5 +1,38 @@
+import "./Events.css";
+import schedule from "../../Data/schedule";
+import { FcAlarmClock, FcCalendar } from "react-icons/fc";
+
 const Events = () => {
-  return <div>Events</div>;
+  return (
+    <section className="events-section" id="events">
+      <h1 className="section-title">events schedule</h1>
+      <div className="events-schedule-container">
+        {schedule.map((currentSchedule) => {
+          return (
+            <div className="event-item">
+              <div className="event-item-day">
+                <div className="event-item-every">
+                  <FcCalendar />
+                  <em>every</em>
+                </div>
+                <span>{currentSchedule.day}</span>
+              </div>
+              <div className="event-item-content">
+                <div className="event-item-name">
+                  <p>{currentSchedule.title}</p>
+                </div>
+                <div className="event-item-location">
+                  <FcAlarmClock />
+                  <span>{currentSchedule.time}</span>
+                </div>
+                <p className="event-item-leader">By {currentSchedule.by}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Events;
